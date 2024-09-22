acbe5b956af0a214e0a8dcd8eb90eff2a4b3443c
Ingrid Narlund <148535265+Ninnindenandra@users.noreply.github.com>
2024-09-22 11:37:39 +0200

    Update Deca110MHurdles.java

--- src/main/java/decathlon/Deca110MHurdles.java
+++ src/main/java/decathlon/Deca110MHurdles.java
@@ -22,7 +22,7 @@ public class Deca110MHurdles {
 				if (runningTime < 10) {
 					System.out.println("Value too low");
 					runningTime = inputResult.enterResult();
-				} else if (runningTime > 28.5) {
+				} else if (runningTime > 30) {
 					System.out.println("Value too high");
 					runningTime = inputResult.enterResult();
 				} else {
